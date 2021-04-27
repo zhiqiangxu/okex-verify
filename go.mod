2807f101a9bf1697b9e6f8d6d32c1fae2c5637f0
徐志强 <652732310@qq.com>
2021-04-27 11:34:37 +0800

    add getProof

--- go.mod
+++ go.mod
@@ -2,10 +2,22 @@ module github.com/zhiqiangxu/okex-verify
 
 go 1.15
 
-require github.com/okex/okexchain-go-sdk v0.16.0
+require (
+	github.com/bartekn/go-bip39 v0.0.0-20171116152956-a05967ea095d
+	github.com/btcsuite/btcd v0.21.0-beta
+	github.com/cosmos/cosmos-sdk v0.39.2
+	github.com/ethereum/go-ethereum v1.9.25
+	github.com/golang/mock v1.5.0
+	github.com/okex/exchain v0.18.2
+	github.com/okex/exchain-go-sdk v0.18.0 // indirect
+	github.com/ontio/ontology-crypto v1.0.9
+	github.com/polynetwork/poly v1.4.0
+	github.com/stretchr/testify v1.7.0
+	github.com/tendermint/tendermint v0.33.9
+)
 
 replace (
-	github.com/cosmos/cosmos-sdk => github.com/okex/cosmos-sdk v0.39.2-okexchain9
-	github.com/tendermint/iavl => github.com/okex/iavl v0.14.1-okexchain1
-	github.com/tendermint/tendermint => github.com/okex/tendermint v0.33.9-okexchain5
+	github.com/cosmos/cosmos-sdk => github.com/okex/cosmos-sdk v0.39.2-exchain3
+	github.com/tendermint/iavl => github.com/okex/iavl v0.14.3-exchain
+	github.com/tendermint/tendermint => github.com/okex/tendermint v0.33.9-exchain2
 )
--- main.go
+++ main.go
@@ -1,25 +1,102 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 
-	sdk "github.com/okex/okexchain-go-sdk"
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/ethclient"
+	oksdk "github.com/okex/exchain-go-sdk"
+	common1 "github.com/polynetwork/poly/common"
+	common2 "github.com/polynetwork/poly/native/service/cross_chain_manager/common"
+	"github.com/polynetwork/poly/native/service/cross_chain_manager/eth"
+	"github.com/zhiqiangxu/okex-verify/pkg/eccm_abi"
+	"github.com/zhiqiangxu/okex-verify/pkg/tools"
 )
 
 var (
-	rpcURL   = "tcp://18.167.142.95:26657"
+	rpcURL   = "https://exchaintestrpc.okex.org"
+	rpcTMURL = "https://exchaintesttmrpc.okex.org"
 	name     = "alice"
 	passWd   = "12345678"
 	mnemonic = "giggle sibling fun arrow elevator spoon blood grocery laugh tortoise culture tool"
 	addr     = "okexchain1ntvyep3suq5z7789g7d5dejwzameu08m6gh7yl"
 )
 
+func getProof() {
+
+	client, err := ethclient.Dial(rpcURL)
+	if err != nil {
+		panic(fmt.Sprintf("ethclient.Dial failed:%v", err))
+	}
+
+	receipt, err := client.TransactionReceipt(context.Background(), common.HexToHash("0x05B02D94644BE47727A4B0FEAC3B8552EE6CFA738AB244CDAD8BA18A82ED766C"))
+	if err != nil {
+		panic(fmt.Sprintf("TransactionReceipt failed:%v", err))
+	}
+	eccmAddr := common.HexToAddress("0x41B323acDdCe4692E4618978bf67DA189C7692d3")
+
+	eccm, err := eccm_abi.NewEthCrossChainManager(eccmAddr, client)
+	if err != nil {
+		panic(fmt.Sprintf("eccm_abi.NewEthCrossChainManager failed:%v", err))
+	}
+	for _, elog := range receipt.Logs {
+
+		if elog.Address == eccmAddr {
+
+			evt, err := eccm.ParseCrossChainEvent(*elog)
+			if err != nil {
+				panic(fmt.Sprintf("eccm.ParseCrossChainEvent failed:%v", err))
+			}
+
+			param := &common2.MakeTxParam{}
+			err = param.Deserialization(common1.NewZeroCopySource([]byte(evt.Rawdata)))
+			if err != nil {
+				panic(fmt.Sprintf("param.Deserialization failed:%v", err))
+			}
+
+			txIDBig := big.NewInt(0)
+			txIDBig.SetBytes(evt.TxId)
+			txID := tools.EncodeBigInt(txIDBig)
+			// txHash := evt.Raw.TxHash.Bytes()
+
+			keyBytes, err := eth.MappingKeyAt(txID, "01")
+			if err != nil {
+				panic(fmt.Sprintf("eth.MappingKeyAt failed:%v", err))
+			}
+
+			refHeight, err := client.BlockNumber(context.Background())
+			if err != nil {
+				panic(fmt.Sprintf("client.BlockNumber failed:%v", err))
+			}
+			height := int64(refHeight - 3)
+			heightHex := hexutil.EncodeBig(big.NewInt(height))
+			proofKey := hexutil.Encode(keyBytes)
+
+			proof, err := tools.GetProof(rpcURL, "0x2a88feB48E176b535da78266990D556E588Cfe06", proofKey, heightHex)
+			if err != nil {
+				panic(fmt.Sprintf("tools.GetProof failed:%v", err))
+			}
+
+			fmt.Println("proof", hex.EncodeToString(proof))
+
+		}
+	}
+}
+
 func main() {
 
-	config, _ := sdk.NewClientConfig(rpcURL, "okexchain-65", sdk.BroadcastBlock, "0.01okt", 200000, 0, "")
-	client := sdk.NewClient(config)
+	getProof()
+	return
 
-	height := int64(1580000)
+	config, _ := oksdk.NewClientConfig(rpcURL, "okexchain-65", oksdk.BroadcastBlock, "0.01okt", 200000, 0, "")
+	client := oksdk.NewClient(config)
+
+	height := int64(2012155)
 	commitResult, err := client.Tendermint().QueryCommitResult(height)
 	if err != nil {
 		panic(err)
